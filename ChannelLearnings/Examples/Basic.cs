41cd71e6d644743164c4390e25865aae78620b0e
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-08-19 15:37:47 +1200

    Initial commit of examples

---
+++ ChannelLearnings/Examples/Basic.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChannelLearnings.Examples
+{
+    public class Basic
+    {
+        public async Task Run()
+        {
+            var channel = Channel.CreateUnbounded<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                await channel.Writer.WriteAsync($"Value: {i} at {DateTime.Now:O}");
+            }
+
+            while (true)
+            {
+                var item = await channel.Reader.ReadAsync();
+                Console.WriteLine(item);
+            }
+        }
+
+        public async Task Run2()
+        {
+            var channel = Channel.CreateUnbounded<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                await channel.Writer.WriteAsync($"Value: {i} at {DateTime.Now:O}");
+            }
+
+            while (await channel.Reader.WaitToReadAsync())            
+                while(channel.Reader.TryRead(out string item))
+                    Console.WriteLine(item);            
+        }
+
+        public async Task Run3()
+        {
+            var channel = Channel.CreateUnbounded<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                await channel.Writer.WriteAsync($"Value: {i} at {DateTime.Now:O}");
+            }
+
+            // takes advantage of the IAsyncEnumerable
+            await foreach(string item in channel.Reader.ReadAllAsync())
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ ChannelLearnings/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ChannelLearnings.Examples;
+
+var workflow = new Workflows();
+
+await workflow.Run();
+
+
+
+Console.WriteLine("Hello, World!");
